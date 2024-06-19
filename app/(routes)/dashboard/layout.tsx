32ae4c5f5bdc1b5688247d0d3978661d60388687
Kayode Olumo <kayode.dev@gmail.com>
2024-06-19 21:16:50 +0100

    sidebar with links

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout: FC<IDashboardLayout> = ({ children }) => {
   return (
     <div>
       <div
-        className="bg-black text-white h-screen md:w-64 fixed top-0 left-0 p-4 rounded-[1rem] m-4"
+        className="bg-stone-950 text-white h-screen md:w-64 hidden md:block fixed top-0 left-0 p-4 rounded-[1rem] m-4"
         style={{ maxHeight: "calc(100vh - 2rem)" }}
       >
         <SideBar />
